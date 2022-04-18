6db8101dd582a1675e84369b148ecac9ca64eb98
homotech <bliam9145@gmail.com>
2022-04-18 09:05:57 +0100

    main

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-function Header(){
-    return(
-        <div>
-            <h1>
-                Adonis Daniel Nnaoma
-            </h1>
-        </div>
-    )
+function Header() {
+  return (
+    <div>
+      <h1>Adonis Daniel Nnaoma</h1>
+    </div>
+  );
 }
+export default Header;
